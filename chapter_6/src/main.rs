a1b5753f58e66755e8f81dc856b2000f5ce4bc77
Angelo Anolin <angelo.anolin@gmail.com>
2021-08-17 00:59:25 -0600

    Chapter 6 Section 3 Iterators and Array codes

--- chapter_6/src/main.rs
+++ chapter_6/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     "Septembeber", "October", "November", "December"
   ];
 
-  let _numbers_list2:[u32;5] = [1, 2, 3, 4, 5];
+  let numbers_list2:[u32;5] = [1, 2, 3, 4, 5];
   let same = ["Hi"; 10];
 
   println!("{}", numbers_list[0]);
@@ -18,5 +18,25 @@ fn main() {
   while idx < 12 {
     println!("The month at index {} is {}", idx, string_list[idx]);
     idx = idx + 1;
+  }
+  
+  // Using for loop
+  for i in &numbers_list2 {
+    println!("{}", i);
+  }
+
+  // Using iterator
+  let string_list_iter = string_list.iter();
+
+  // The variable needs to be cloned as once it is used
+  // the reference to the heap where the variable points to 
+  // is already gone.
+  let string_list_iter_clone = string_list_iter.clone();
+
+  println!("variable string_list has {} elements.", string_list_iter.count());
+
+  
+  for j in string_list_iter_clone {
+    println!("Month {} has {} characters", j, j.len());
   }
 }
