0f03de142e7eb861883e17e305f95ca68b4051e5
Angelo Anolin <angelo.anolin@gmail.com>
2021-08-16 22:23:32 -0600

    Chapter 6 Section 2 codes

--- chapter_6/src/main.rs
+++ chapter_6/src/main.rs
@@ -6,9 +6,17 @@ fn main() {
     "Septembeber", "October", "November", "December"
   ];
 
-  let numbers_list2:[u32;5] = [1, 2, 3, 4, 5];
+  let _numbers_list2:[u32;5] = [1, 2, 3, 4, 5];
   let same = ["Hi"; 10];
 
   println!("{}", numbers_list[0]);
   println!("{:?}", same);
+
+  // Traversing the array
+  let mut idx= 0;
+  
+  while idx < 12 {
+    println!("The month at index {} is {}", idx, string_list[idx]);
+    idx = idx + 1;
+  }
 }
